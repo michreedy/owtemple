1900de33bad3ee66e10e362c5c11d23907ed1171
Mich Reedy <mickolas@build-1-1.cinci.rr.com>
2019-08-27 03:23:04 -0400

    fb19

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,20 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 const express = require("express");
 const app = express();
 var bodyParser = require("body-parser");
 
-
 admin.initializeApp();
 
-
+var firebaseConfig = {
+  apiKey: "AIzaSyB0fg5dGrlZ6y1GKo0wltHL3I6IvdSUNAM",
+  authDomain: "https://owtemple.com",
+  databaseURL: "https://ow-temple.firebaseio.com",
+  projectId: "ow-temple",
+  storageBucket: "",
+  messagingSenderId: "844287921405",
+  appId: "1:844287921405:web:d43912a7eaa1b698"
+};
 
 FirebaseFirestore.initializeApp(firebaseConfig);
 var db = firebase.firestore();
@@ -32,5 +39,3 @@ app.get("/account/:name", (req, res) => {
   res.send(new Buffer(`<h1>hi, ${name}, your email is ${user.email}</h1>`));
 });
 app.post("/new-account", myPostFunction);
-
-app.listen(port, (3000) => console.log(`Example app listening on port ${port}!`));
